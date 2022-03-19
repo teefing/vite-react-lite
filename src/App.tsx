1f25ef1395c40ad7b0449c2e71bc18a8ab613621
teefing <tengfeitf@foxmail.com>
2022-03-19 16:29:44 +0800

    chore: update

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="text-20 text-red-300">
-      <text onClick={() => setCount(count + 1)}>hello world {count}</text>
+    <div onClick={() => setCount(count + 1)} className="w-screen h-screen text-20 text-red-300 flex justify-center items-center">
+      <text className="text-[50px] select-none" >Day {count}</text>
     </div>
   )
 }
